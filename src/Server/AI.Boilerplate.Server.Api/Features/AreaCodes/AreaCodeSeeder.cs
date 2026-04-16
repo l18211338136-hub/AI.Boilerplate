85d37fe7931ab738192cb16169a8d6d2daaf0ab2
LYP <14142744+lyp_conding@user.noreply.gitee.com>
2026-04-16 19:45:24 +0800

    feat(AreaCodes): 实现基于 SQL 文件的行政区域地址数据初始化

---
+++ src/Server/AI.Boilerplate.Server.Api/Features/AreaCodes/AreaCodeSeeder.cs
@@ -0,0 +1,23 @@
+﻿using AI.Boilerplate.Server.Api.Infrastructure.Data.Seed;
+
+namespace AI.Boilerplate.Server.Api.Features.AreaCodes;
+
+public class AreaCodeSeeder : IDataSeeder
+{
+    public async Task SeedAsync(AppDbContext dbContext, CancellationToken cancellationToken)
+    {
+        if (await dbContext.AreaCodes.AnyAsync(cancellationToken) is false)
+        {
+            var sqlFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "AreaCodes.sql");
+            if (File.Exists(sqlFile))
+            {
+                var sqlScript = await File.ReadAllTextAsync(sqlFile);
+                await dbContext.Database.ExecuteSqlRawAsync(sqlScript, cancellationToken);
+            }
+            else
+            {
+                Console.WriteLine($"SQL file not found: {sqlFile}");
+            }
+        }
+    }
+}
